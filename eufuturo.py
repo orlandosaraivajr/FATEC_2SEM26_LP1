d9a443b199bdb497a0a637bab023cf5c21b30c45
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2026-08-21 12:22:32 -0300

    Terceiro Programa

---
+++ eufuturo.py
@@ -0,0 +1,9 @@
+idade = input("Digite sua idade ")
+# Convertendo a variável idade do tipo
+# string (str) para inteiro
+idade = int(idade)
+idade_futura = idade + 5
+# Convertendo a variável idade_futura do tipo
+#inteiro (int) para string (str)
+idade_futura = str(idade_futura)
+print("Sua idade " + idade_futura)
