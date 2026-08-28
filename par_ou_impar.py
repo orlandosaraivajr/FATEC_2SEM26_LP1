902701339d4bb7fa494c242513498270dad05254
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2026-08-28 11:29:03 -0300

    Exemplos em sala

---
+++ par_ou_impar.py
@@ -0,0 +1,14 @@
+numero = int(input('Digite um número'))
+
+# Descobrir se o número é par ou impar
+# Pego o número, calculo o resto da divisão
+# SE for igual a zero, é um número par
+# Se for igual a um, é um número ímpar
+
+if numero % 2 == 0 :
+    print("Calculado o número")
+    print(" É par")
+
+if numero % 2 == 1:
+    print('Calculado o numero e descobri que')
+    print('Este número é ímpar')
